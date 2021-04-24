84ac83a9990749720e38b28785031d48df19bae6
Nkululeko Mbhele <nkululekombhele6017@gmail.com>
2021-04-24 14:24:08 +0200

    initial commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+
+#include "Student.h"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	    Student s1 {"Cecelia", 3.8 };
+	        Student s2 {"Sam", 3.1};
+
+		    cout << s1 << endl;
+		        cout << s2 << endl;
+}
